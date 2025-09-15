5c86a7661d12627f21466372584b91a4fbb63145
Th1stsay <vicemail236@gmail.com>
2025-09-15 19:35:08 +0300

    add slider to reviews

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,15 @@
 var swiper = new Swiper(".beforeAfterSlider", {
-   navigation: {
-      nextEl: ".right",
-      prevEl: ".left",
-   },
+  navigation: {
+    nextEl: ".right",
+    prevEl: ".left",
+  },
+});
+
+var swiper = new Swiper(".SliderReviews", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".right",
+    prevEl: ".left",
+  },
 });
