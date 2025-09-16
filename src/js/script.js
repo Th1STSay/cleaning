0e5af84095c0bfe45a22030c2ee8bc32867d8c60
Th1stsay <vicemail236@gmail.com>
2025-09-16 21:52:38 +0300

    add 2 swipers

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,43 @@ var swiper = new Swiper(".SliderReviews", {
     prevEl: ".left",
   },
 });
+
+var swiper = new Swiper(".horizontalSwiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var menuButton = document.querySelector(".burgerSwiper-content-wrapper-burger");
+var openMenu = function () {
+  swiper.slidePrev();
+};
+var swiper = new Swiper(".BurgerSwiper", {
+  slidesPerView: "auto",
+  initialSlide: 1,
+  resistanceRatio: 0,
+  slideToClickedSlide: true,
+  on: {
+    slideChangeTransitionStart: function () {
+      var slider = this;
+      if (slider.activeIndex === 0) {
+        menuButton.classList.add("cross");
+        // required because of slideToClickedSlide
+        menuButton.removeEventListener("click", openMenu, true);
+      } else {
+        menuButton.classList.remove("cross");
+      }
+    },
+    slideChangeTransitionEnd: function () {
+      var slider = this;
+      if (slider.activeIndex === 1) {
+        menuButton.addEventListener("click", openMenu, true);
+      }
+    },
+  },
+});
