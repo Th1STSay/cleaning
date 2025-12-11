a854c0df64772cc08a7276bc264da4034c529931
Th1stsay <vicemail236@gmail.com>
2025-12-11 22:51:51 +0200

    adaptive + burgerMenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 var swiper = new Swiper(".beforeAfterSlider", {
   slidesPerView: 1,
-  spaceBetween: 0, // без відступів (щоб не було видно другого)
-  centeredSlides: false, // НЕ центруємо (бо центрування показує частину сусіднього слайда)
+  spaceBetween: 30, // без відступів (щоб не було видно другого)
+  centeredSlides: true,
+  Loop: true, // НЕ центруємо (бо центрування показує частину сусіднього слайда)
   navigation: {
     nextEl: ".right",
     prevEl: ".left",
@@ -21,61 +22,22 @@ var swiper = new Swiper(".SliderReviews", {
       slidesPerView: 3,
     },
     1200: {
-      slidesPerView: 2,
+      slidesPerView: 3,
     },
     768: {
+      slidesPerView: 2,
+    }, 
+    576: {
+      slidesPerView: 1,
+    },
+    320: {
       slidesPerView: 1,
     },
   },
 });
 
-// var swiper = new Swiper(".horizontalSwiper", {
-//   direction: "horizontal",
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   mousewheel: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
 
-// var menuButton = document.querySelector(".burgerSwiper__content-wrapper-burger");
-// var openMenu = function () {
-//   swiper.slidePrev();
-// };
-
-// // Додаємо обробник події при завантаженні сторінки
-// menuButton.addEventListener('click', openMenu, true);
-
-// var swiper = new Swiper(".BurgerSwiper", {
-//   slidesPerView: "auto",
-//   initialSlide: 1,
-//   resistanceRatio: 0,
-//   slideToClickedSlide: true,
-//   on: {
-//     init: function () {
-//       // Перевіряємо початковий стан
-//       if (this.activeIndex === 0) {
-//         menuButton.classList.add("cross");
-//         menuButton.removeEventListener("click", openMenu, true);
-//       }
-//     },
-//     slideChangeTransitionStart: function () {
-//       var slider = this;
-//       if (slider.activeIndex === 0) {
-//         menuButton.classList.add("cross");
-//         // required because of slideToClickedSlide
-//         menuButton.removeEventListener("click", openMenu, true);
-//       } else {
-//         menuButton.classList.remove("cross");
-//       }
-//     },
-//     slideChangeTransitionEnd: function () {
-//       var slider = this;
-//       if (slider.activeIndex === 1) {
-//         menuButton.addEventListener("click", openMenu, true);
-//       }
-//     },
-//   },
-// });
+function openBurger() {
+  let menu = document.querySelector(".header__navigation-menu");
+  menu.classList.toggle("active");
+}
